9c5204e86bd7219db41a6045fc858a3b3898ca50
Junki Yonezu <yonegen0@gmail.com>
2024-09-16 10:14:20 +0900

    フォームデザイン修正

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
 from wtforms import IntegerField, SubmitField, DateField
 from wtforms.form import Form
+import datetime
+from dateutil.relativedelta import relativedelta
 
 import my_flask.chart_analysis as chart
 
 app = Flask(__name__, static_folder="my_flask")
 
 class StockInfForm(Form):
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(days=1)
+    one_year_ago = yesterday - relativedelta(years=1)
     stockCode = IntegerField('銘柄コード')
     enterButton = SubmitField('検索')
     startDay = DateField('分析開始日')
@@ -21,6 +26,8 @@ def stock_form():
             startDay = stockForm.startDay.data
             finishDay = stockForm.finishDay.data
             stockForm = StockInfForm()
+            
+            print("銘柄コード：",stockCode)
         else:
             print('入力に誤りがあります')
     return render_template('form.html', form=stockForm)
